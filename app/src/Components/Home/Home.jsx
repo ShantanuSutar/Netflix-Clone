e4e4249848210f1d4c966e3a9517ee1c7f0049a8
Shantanu Sutar <shantanusutar17@gmail.com>
2023-03-19 10:28:21 +0530

    Displaying the fetched images of movies

--- app/src/Components/Home/Home.jsx
+++ app/src/Components/Home/Home.jsx
@@ -1,33 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.scss";
 import axios from "axios";
 
 const apiKey = "0c3b5bdc594113fd74bd533ce2f43aa0";
 const url = "https://api.themoviedb.org/3";
+const imgUrl = "https://image.tmdb.org/t/p/original";
 const upcoming = "upcoming";
 
 const Card = ({ img }) => {
   return (
     <div>
-      <img className="card" src={img} alt="" />;
+      <img className="card" src={img} alt="" />
     </div>
   );
 };
 
-const Row = ({
-  title,
-  arr = [
-    {
-      img: "https://images.moviesanywhere.com/6305a9e8ed76d5fa485ac16637655cf7/bcc68be4-eede-409b-a63d-e179b28d19b4.jpg",
-    },
-  ],
-}) => {
+const Row = ({ title, arr = [] }) => {
   return (
     <div className="row">
       <h2>{title}</h2>
       <div>
         {arr.map((item, index) => {
-          <Card key={index} img={item.img} />;
+          return <Card key={index} img={`${imgUrl}${item.poster_path}`} />;
         })}
       </div>
     </div>
@@ -35,19 +29,21 @@ const Row = ({
 };
 
 const Home = () => {
+  const [upcomingMovies, setUpcomingMovies] = useState([]);
   useEffect(() => {
     const fetchUpcoming = async () => {
       const {
         data: { results },
       } = await axios.get(`${url}/movie/${upcoming}?api_key=${apiKey}`);
-      console.log(results);
+      setUpcomingMovies(results);
+      console.log(upcomingMovies);
     };
     fetchUpcoming();
   }, []);
   return (
     <section className="home">
       <div className="banner"></div>
-      <Row title={"Popular on Netflix"} />
+      <Row title={"Popular on Netflix"} arr={upcomingMovies} />
       <Row title={"Movies"} />
       <Row title={"TV Shows"} />
       <Row title={"Recently Viewed"} />
